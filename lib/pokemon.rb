97397b13779e5179c9ff43e5146ff077081e78ec
Benjamin Cheng <bencheng200@gmail.com>
2020-12-13 20:23:10 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,7 @@ class Pokemon
       db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
   end
 
-  def self.find 
+  def self.find
     db.execute("SELECT * FROM pokemon WHERE id = ?", id)
   end
 end
